de62681fa96aaf78b373238f55675fd06ff71bc6
LarsWinterss <Lucas.Winkler@stud.uni-regensburg.de>
2022-01-10 12:40:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Case 5 BIS - Lucas Winkler
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
